4901c7e3abdccc01be7cd951129d145911a0c737
Mathewski77 <mlacsina23-0408@cca.edu.ph>
2025-02-19 19:18:20 +0800

    Update table.php

--- table.php
+++ table.php
@@ -5,14 +5,19 @@
 
     <body>
         <h1>Student Record</h1>
-        <table id="student_record"> </table>
+        <table id="student_record" border="1"> </table>
         <script>
             const studentRecord = document.getElementById
             ("student_record");
+
             console.log("Hello World");
             const row1 = studentRecord.insertRow();
             const row1col1= row1.insertCell();
             row1col1.innerHTML = "LastName";
+
+            const row1col2= row1.insertCell();
+            row1col2.innerHTML = "FirstName";
+
             console.log(studentRecord);
         </script>
         </form>
